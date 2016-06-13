19bb6984352d19f48577ea03a59fa52b3a3f50d8
Jos Boumans <jos@dwim.org>
2016-06-13 11:56:32 -0700

    * OS detection

--- test/run_httpd.sh
+++ test/run_httpd.sh
@@ -7,4 +7,14 @@ if [ ! -x apache2ctl ]; then
   BIN=apachectl
 fi
 
-$BIN -d `pwd` -f `pwd`/test/httpd.conf -X -k start
+### What config file do we load? subtle differences per OS :(
+if[ `grep -i ubuntu /etc/issue` -eq 0 ]; then
+    CONF='ubuntu'
+elif [ `grep -i centos /etc/issue` -eq 0 ]; then
+    CONF='centos'
+else
+    echo "Unkonwn OS - please update this script for better detection"
+    exit 1;
+fi
+
+$BIN -d `pwd` -f `pwd`/test/httpd.conf.$CONF -X -k start
