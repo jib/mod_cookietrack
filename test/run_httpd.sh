8777fae4f584a395a18a32baadad6248174a09dd
Jos Boumans <jos@dwim.org>
2016-06-13 11:36:08 -0700

    * detect version of apache bin

--- test/run_httpd.sh
+++ test/run_httpd.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-apache2ctl -d `pwd` -f `pwd`/test/httpd.conf -X -k start
+BIN=apache2ctl
+
+### If you're on an older OS, you may not have apache2ctl, so use a
+### fallback.
+if [ ! -x apache2ctl ]; then
+  BIN=apachectl
+fi
+
+$BIN -d `pwd` -f `pwd`/test/httpd.conf -X -k start
